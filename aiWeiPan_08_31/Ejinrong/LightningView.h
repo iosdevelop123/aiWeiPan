0b08bd548971fc2d7449db3302d8751f0195758f
iosdevelop123 <543626290@qq.com>
2016-09-06 16:07:22 +0800

    项目提交

---
+++ aiWeiPan_08_31/Ejinrong/LightningView.h
@@ -0,0 +1,21 @@
+//
+//  LightningView.h
+//  lightningViewDemo
+//
+//  Created by dayu on 15/11/3.
+//  Copyright © 2015年 dayu. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface LightningView : UIView
+
+@property (strong,nonatomic) NSMutableArray *pointArray;//波动点数组
+@property (strong,nonatomic) NSMutableArray *priceArray;//波动价数组
+@property (strong,nonatomic) UIImageView *bgImageView;//六虚线背景图片视图
+@property (strong,nonatomic) UILabel *priceLabel;//闪电图买价Label
+@property (assign,nonatomic) CGFloat maxPriceForLightningView;
+@property (assign,nonatomic) CGFloat minPriceForLightningView;
+
+- (void)refreshPoint:(NSMutableArray *)pointArray;
+
+@end
